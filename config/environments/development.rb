a1786d4dc4e3a320505afe67420ff0be21b77501
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-05 23:37:13 +0000

    [Update]グループにメールを送信する機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -74,4 +74,15 @@ Rails.application.configure do
   config.hosts.clear
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            '送信元となるGmailアドレスを記入',
+    password:             'アプリパスワードを記入',
+    authentication:       'login',
+    enable_starttls_auto:  true
+}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index,:show,:edit,:update]
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
